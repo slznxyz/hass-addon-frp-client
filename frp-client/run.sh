7145e7ad71ecd5ad5e5ddf3424e6aef88a9394ac
slznxyz <92085057+slznxyz@users.noreply.github.com>
2025-06-26 22:41:08 +0800

    Update run.sh

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -14,7 +14,7 @@ sed -i "s/serverAddr = \"vip.slzn999.tk\"/serverAddr = \"$(bashio::config 'serve
 sed -i "s/serverPort = 17010/serverPort = $(bashio::config 'serverPort')/" $CONFIG_PATH
 sed -i "s/user = \"user1\"/user = \"$(bashio::config 'user')\"/" $CONFIG_PATH
 sed -i "s/metadatas.token = \"123456789\"/metadatas.token = \"$(bashio::config 'metadatastoken')\"/" $CONFIG_PATH
-sed -i "s/subDomains = \[\"your_domain\"\]/subDomains = [\"$(bashio::config 'customDomain')\"]/" $CONFIG_PATH
+sed -i "s/subDomains = \[\"your_domain\"\]/subDomains = [\"$(bashio::config 'subDomain')\"]/" $CONFIG_PATH
 sed -i "s/name = \"your_proxy_name\"/name = \"$(bashio::config 'proxyName')\"/" $CONFIG_PATH
 
 
